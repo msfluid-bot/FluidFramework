ffdec8d7cb0b255ad67d050e42e18a4a1b927aa0
Tyler Butler <tyler@tylerbutler.com>
2023-02-14 15:09:39 -0800

    tools: add scopes to commitlint config

--- build-tools/commitlint.config.cjs
+++ build-tools/commitlint.config.cjs
@@ -4,90 +4,134 @@
  */
 const { getPackagesSync } = require("@manypkg/get-packages");
 const { PackageName } = require("@rushstack/node-core-library");
+const { execSync } = require("child_process");
 
 const { packages } = getPackagesSync(process.cwd());
 const scopes = packages.map((pkg) => PackageName.getUnscopedName(pkg.packageJson.name));
-// const scopeEnum = scopes.map((scope) => return {scope: })
+const types = [
+	{
+		value: "build",
+		title: "Builds",
+		description: "Changes that affect the build system or external dependencies",
+		emoji: "🏗️",
+	},
+	{
+		value: "ci",
+		description: "Changes to our CI configuration files and scripts",
+		title: "Continuous Integrations",
+		emoji: "🤖",
+	},
+	{
+		value: "deprecate",
+		description:
+			"Changes which mark one or more APIs as being deprecated and make no functional changes",
+		title: "Deprecations",
+		emoji: "🔙",
+	},
+	{
+		value: "docs",
+		description: "Changes which strictly modify documentation",
+		title: "Documentation",
+		emoji: "📚",
+	},
+	{
+		value: "feat",
+		description: "Adds a new feature",
+		title: "Features",
+		emoji: "✨",
+	},
+	{
+		value: "fix",
+		description: "A bug fix",
+		title: "Bug Fixes",
+		emoji: "🐛",
+	},
+	{
+		value: "improvement",
+		description:
+			"Improves an existing implementation without adding a new feature or fixing a bug",
+		title: "Improvement",
+		emoji: "👍",
+	},
+	{
+		value: "merge",
+		description: "Merge of branches that does nothing more than merge",
+		title: "Merge",
+		emoji: "🛣️",
+	},
+	{
+		value: "perf",
+		description: "A code change aimed at improving performance",
+		title: "Performance Improvements",
+		emoji: "🚀",
+	},
+	{
+		value: "refactor",
+		description:
+			"Code Refactoring - a code change to improve code quality that does not affect runtime behavior",
+		title: "Code Refactoring",
+		emoji: "📦",
+	},
+	{
+		value: "remove",
+		description: "Removes a feature",
+		title: "Remove",
+		emoji: "💥",
+	},
+	{
+		value: "revert",
+		description: "Changes which strictly revert a previous change(s)",
+		title: "Reverts",
+		emoji: "🗑",
+	},
+	{
+		value: "style",
+		description:
+			"Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons)",
+		title: "Style",
+		emoji: "💎",
+	},
+	{
+		value: "test",
+		description: "Adds missing tests or correcting existing tests",
+		title: "Tests",
+		emoji: "🧪",
+	},
+	{
+		value: "tools",
+		description:
+			"Changes to repository tooling (scripts, linter configurations) that do not affect published packages",
+		title: "Tools",
+		emoji: "🛠",
+	},
+];
 
+/** @type {import('cz-git').UserConfig} */
 module.exports = {
-	extends: ["@commitlint/config-conventional"],
 	rules: {
-		"body-case": [2, "always", "sentence-case"],
-		"subject-case": [2, "always", "sentence-case"],
-		"type-enum": [
-			2,
-			"always",
-			["build", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"],
-		],
-		"scope-enum": [
-			2,
-			"always",
-			scopes,
-		],
+		// @see: https://commitlint.js.org/#/reference-rules
+		"body-case": [2, "always", ["sentence-case", "lower-case"]],
+		"subject-case": [2, "always", ["sentence-case", "lower-case"]],
+		"type-enum": [2, "always", types.map((v) => v.value)],
+		"scope-enum": [2, "always", scopes],
+		"header-max-length": [2, "always", 100],
 	},
 	prompt: {
-		questions: {
-			type: {
-				description: "Select the type of change that you're committing",
-				enum: {
-					build: {
-						description:
-							"Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)",
-						title: "Builds",
-						emoji: "🛠",
-					},
-					ci: {
-						description:
-							"Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)",
-						title: "Continuous Integrations",
-						emoji: "⚙️",
-					},
-					docs: {
-						description: "Documentation only changes",
-						title: "Documentation",
-						emoji: "📚",
-					},
-					feat: {
-						description: "A new feature",
-						title: "Features",
-						emoji: "✨",
-					},
-					fix: {
-						description: "A bug fix",
-						title: "Bug Fixes",
-						emoji: "🐛",
-					},
-					perf: {
-						description: "A code change that improves performance",
-						title: "Performance Improvements",
-						emoji: "🚀",
-					},
-					refactor: {
-						description: "A code change that neither fixes a bug nor adds a feature",
-						title: "Code Refactoring",
-						emoji: "📦",
-					},
-					revert: {
-						description: "Reverts a previous commit",
-						title: "Reverts",
-						emoji: "🗑",
-					},
-					style: {
-						description:
-							"Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)",
-						title: "Style",
-						emoji: "💎",
-					},
-					test: {
-						description: "Adding missing tests or correcting existing tests",
-						title: "Tests",
-						emoji: "🚨",
-					},
-				},
-			},
-      scope: {
-        enum: scopes,
-      }
+		// alias: { fd: "docs: fix typos" },
+		messages: {
+			value: "Select the type of change that you're committing:",
+			scope: "Denote the SCOPE of this change (optional):",
+			customScope: "Denote the SCOPE of this change:",
+			subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
+			body: 'Provide a LONGER description of the change (optional). Use "|" for line breaks:\n',
+			breaking: 'List any BREAKING CHANGES (optional). Use "|" for line breaks:\n',
+			confirmCommit: "Are you sure you want to proceed with the commit above?",
 		},
+		types: types.map((v) => {
+			return { value: v.value, name: `${v.value}: ${v.description}` };
+		}),
+		allowCustomScopes: true,
+		allowEmptyScopes: true,
+		allowBreakingChanges: ["feat", "fix", "refactor", "tools", "perf", "remove"],
 	},
 };
